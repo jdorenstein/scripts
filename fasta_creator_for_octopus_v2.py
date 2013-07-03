4808b160859b065784faf25f1c28c760b2ee7f2b
Jeffery Orenstein <ionchannel@Erics-iMac.local>
2013-07-03 13:10:25 -0700

    changed how the script handles a fasta file. now, it lists the best human hit in a much more efficient and readable manner

--- fasta_creator_for_octopus_v2.py
+++ fasta_creator_for_octopus_v2.py
@@ -1,6 +1,6 @@
 #!C:\Python27 
 
-#script: fasta_creator_for_octopus_v1.0
+#script: fasta_creator_for_octopus_v2.0
 #function: To create a fasta database for the entire octopus genome. 
 
 
@@ -12,7 +12,7 @@
 #input a CSV file from the octopus genome database
 #file format: [PFAM description	genes with this PFAM	PANTHER	PANTHER description	genes with this PANTHER	best human hit	human hit description	peptide]
 
-IOcsv_file = open ( '/Users/ionchannel/research/db/blast/oct.proteome/origional.docs/oct_v2_fasta.txt', 'r' )
+IOcsv_file = open ( '/Users/ionchannel/research/tools/db/blast/oct.proteome/000.origional.docs/oct_v2_fasta.txt', 'r' )
 
 ###IOout
 
@@ -20,7 +20,7 @@ IOcsv_file = open ( '/Users/ionchannel/research/db/blast/oct.proteome/origional.
 #output fasta database that is formatted
 #file format: [>[pacid] Hsa-[best known human hit]]
 
-IOfasta_out = open( '/Users/ionchannel/research/db/blast/oct.proteome/origional.docs/octProteome.fa', 'a' )
+IOfasta_out = open( '/Users/ionchannel/research/tools/db/blast/oct.proteome/000.origional.docs/octProteome.fa', 'a' )
 
 ###parse 1: all query - All data
 
@@ -32,14 +32,16 @@ for line in IOcsv_file:
     #print line + '\n \n \n \n'
     pacID = lineSplit[ 0 ]
     bhh = lineSplit [ 12 ]
+    bhhd = lineSplit[ 13 ]
     peptide = lineSplit [ 14 ]
-	
-	#format new addition into database file
+    output = '>' + pacID + ' ' #information is always present
+	#append bhh onto output. this information may or may not be present
     if bhh:
-            output = ">" + pacID + " Hsa-" + bhh + '\n' + peptide + '\n' #note that the output prints to TWO lines
-
+        output = output + 'Hsa-' + bhh + '\n' + peptide + '\n' 
     if not bhh:
-            output = ">" + pacID + " Hsa-" + "NO NAME FOR PROTEIN" + '\n' + peptide + '\n' #note that the output prints to TWO lines
+        output = output + 'No best human hit identifed in Excel file' + '\n' + peptide + '\n'
+ 
+ 
 	#append into fasta database file
 	
     IOfasta_out.write( output )
