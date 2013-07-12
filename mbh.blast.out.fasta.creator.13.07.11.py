e248f91b428f7faaba8c5bb09f54dfabab457dd2
Jeffery Orenstein <ionchannel@Erics-iMac.local>
2013-07-12 11:05:28 -0700

    rewrote the mbh.blast.out.fasta.creator.13.07.11.py script. now it creates a list of the top hits and appends the human query onto the list

--- mbh.blast.out.fasta.creator.13.07.11.py
+++ mbh.blast.out.fasta.creator.13.07.11.py
@@ -32,80 +32,81 @@ blastE001_out = open ( '/Users/ionchannel/research/projects/ionchannels/non-sele
 blastE10_list = []
 blastE1_list = []
 blastE001_list = []
-
-
+dict_key = 1
+blastE10_hsa = []
+blastE1_hsa = []
+blastE001_hsa = []
+is_top = True
 #read in the blast reports (uses same algorithim three times)
 
-for line in blastE10_in:
+for line in blastE10_in: #split the blast report up on the lines
 	lineSplit = line.split(' ')
-	
-	#this identifies a hit in the blast report then appends it to a list of pacID's (the pacID's of the hits)
-	if lineSplit[0][0:4] == 'lcl|': 
-		blastE10_list.append(lineSplit[0][4:]) 
-
+	if lineSplit[0] == 'Query=': #if the script finds a query, it appends the query's id onto the blast_hsa array's. then, it sets is_top to true, which tells the script to record the first top hit it finds
+		blastE10_hsa.append(lineSplit[1])
+		is_top = True
+	#if the line is a result and is_top is True, then the result's id is appended onto blast_list
+	if lineSplit[0][0:4] == 'lcl|' and is_top == True:
+		blastE10_list.append(lineSplit[0][4:])
+		is_top = False
+		
 for line in blastE1_in:
 	lineSplit = line.split(' ')
-	
-	#this identifies a hit in the blast report then appends it to a list of pacID's (the pacID's of the hits)
-	if lineSplit[0][0:4] == 'lcl|': 
+	if lineSplit[0] == 'Query=': #if the script finds a query, it appends the query's id onto the blast_hsa array's. then, it sets is_top to true, which tells the script to record the first top hit it finds
+		blastE1_hsa.append(lineSplit[1])
+		is_top = True
+	#if the line is a result and is_top is True, then the result's id is appended onto blast_list
+	if lineSplit[0][0:4] == 'lcl|' and is_top == True: 
 		blastE1_list.append(lineSplit[0][4:]) 
-		
+		is_top = False
+			
 for line in blastE001_in:
 	lineSplit = line.split(' ')
-	
-	#this identifies a hit in the blast report then appends it to a list of pacID's (the pacID's of the hits)
-	if lineSplit[0][0:4] == 'lcl|': 
+	if lineSplit[0] == 'Query=': #if the script finds a query, it appends the query's id onto the blast_hsa array's. then, it sets is_top to true, which tells the script to record the first top hit it finds
+		blastE001_hsa.append(lineSplit[1])
+		is_top = True
+	#if the line is a result and is_top is True, then the result's id is appended onto blast_list
+	if lineSplit[0][0:4] == 'lcl|' and is_top == True: 
 		blastE001_list.append(lineSplit[0][4:]) 
+		is_top = False
 
-
-#make every entry in the lists unique
-
-blastE10_list = list(set(blastE10_list))
-blastE1_list = list(set(blastE1_list))
-blastE001_list = list(set(blastE001_list))
 
 ###parse 2###
 
 ##for each entry in the list, fetch the corresponding octopus fasta entry
 
+#new method for parsing the CSV file
+
 #declare variables
 
-#read every line of csv file, then test to see if the pacID is in a blast_list. if there is a match, append the fasta entry into the fasta file
-
+octFasta = {}
+#create a dictionary from the CSV file that uses the pacID as the key
 for line in octFasta_in:
 	lineSplit = line.split('\t')
-	pacID = lineSplit[ 0 ]
-	if pacID in blastE1_list:
-		bhh = lineSplit[12]
-		peptide = lineSplit[14]
-		output = '>' + pacID + ' ' #information is always present
-		#append bhh onto output (this info may or may not be present
-		if bhh:
-			output = output + 'Hsa-' + bhh + '\n' + peptide + '\n'
-		if not bhh:
-			output = output + 'No best human hit identified in Excel file' + '\n' + peptide + '\n'
-		blastE1_out.write(output)
-	if pacID in blastE001_list:
-		bhh = lineSplit[12]
-		peptide = lineSplit[14]
-		output = '>' + pacID + ' ' #information is always present
-		#append bhh onto output (this info may or may not be present
-		if bhh:
-			output = output + 'Hsa-' + bhh + '\n' + peptide + '\n'
-		if not bhh:
-			output = output + 'No best human hit identified in Excel file' + '\n' + peptide + '\n'
-		blastE001_out.write(output)
-	if pacID in blastE10_list:
-		bhh = lineSplit [12]
-		peptide = lineSplit[14]
-		output = '>' + pacID + ' ' #information is always present
-		#append bhh onto output (this info may or may not be present
-		if bhh:
-			output = output + 'Hsa-' + bhh + '\n' + peptide + '\n'
-		if not bhh:
-			output = output + 'No best human hit identified in Excel file' + '\n' + peptide + '\n'
-		blastE10_out.write(output)
-    
+	bhh = lineSplit [12]
+	peptide = lineSplit[14]
+	
+	if bhh:
+		bhh_mod = 'Hsa-' + bhh + ': '
+		octFasta[lineSplit[0]] = [lineSplit[0] , bhh_mod , peptide ]
+	if not bhh:
+		octFasta[lineSplit[0]] = [lineSplit[0] , 'No best human hit identified in Excel file: ' , peptide ] 
+
+#now, for each of the blast_list files, the script will use the entry as a key in the octopus fasta file to create the formatted output
+num = 0
+for item in blastE10_list:	
+	output = '>' + octFasta[item][0] + ' ' + octFasta[item][1] + blastE10_hsa[num] + '\n' + octFasta[item][2] 
+	blastE10_out.write(output)
+	num = num + 1
+num = 0
+for item in blastE1_list:
+	output = '>' + octFasta[item][0] + ' ' + octFasta[item][1] + blastE1_hsa[num] + '\n' + octFasta[item][2]
+	blastE1_out.write(output)
+	num = num + 1
+num = 0
+for item in blastE001_list:
+	output = '>' + octFasta[item][0] + ' ' + octFasta[item][1] + blastE001_hsa[num] + '\n' + octFasta[item][2]
+	blastE001_out.write(output)
+	num = num + 1
 
 print 'done'
  
