b877d5991354190a5f871015c80c13e01b91aac5
Jeffery Orenstein <ionchannel@Erics-iMac.local>
2013-07-12 11:54:41 -0700

    added more code to mbh.sorter.13.07.12.py. unsure if it is a working version. will recommit if it is a working version

--- mbh.sorter.13.07.12.py
+++ mbh.sorter.13.07.12.py
@@ -5,18 +5,18 @@
 ###IOin
 #frmt: query= pacid hsa-xxx: hsa_input_query
 #frmt: lcl|hsa_query_top_hit
-blastE10_in = open( 'Users/ionchannel/research/projects/ionchannels/non-selective.k/geneset/out.blast.mbh.007' , 'r' )
-blastE1_in = open( 'Users/ionchannel/research/projects/ionchannels/non-selective.k/geneset/out.blast.mbh.008' , 'r' )
-blastE001_in = open( 'Users/ionchannel/research/projects/ionchannels/non-selective.k/geneset/out.blast.mbh.009' , 'r' )
+blastE10_in = open( '/Users/ionchannel/research/projects/ionchannels/non-selective.k/geneset/out.blast.mbh.007' , 'r' )
+blastE1_in = open( '/Users/ionchannel/research/projects/ionchannels/non-selective.k/geneset/out.blast.mbh.008' , 'r' )
+blastE001_in = open( '/Users/ionchannel/research/projects/ionchannels/non-selective.k/geneset/out.blast.mbh.009' , 'r' )
 
 ###IOout
 
-mbhE10_out = open( 'Users/ionchannel/research/projects/ionchannels/non-selective.k/geneset/out.mbh.e_val10','a')
-notE10_out = open( 'Users/ionchannel/research/projects/ionchannels/non-selective.k/geneset/out.not.mbh.e_val10','a')
-mbhE1_out = open( 'Users/ionchannel/research/projects/ionchannels/non-selective.k/geneset/out.mbh.e_val1','a')
-notE1_out = open( 'Users/ionchannel/research/projects/ionchannels/non-selective.k/geneset/out.not.mbh.e_val1','a')
-mbhE001_out = open( 'Users/ionchannel/research/projects/ionchannels/non-selective.k/geneset/out.mbh.e_val0.01','a')
-notE001_out = open( 'Users/ionchannel/research/projects/ionchannels/non-selective.k/geneset/out.not.mbh.e_val0.01','a')
+mbhE10_out = open( '/Users/ionchannel/research/projects/ionchannels/non-selective.k/geneset/out.mbh.e_val10','a')
+notE10_out = open( '/Users/ionchannel/research/projects/ionchannels/non-selective.k/geneset/out.not.mbh.e_val10','a')
+mbhE1_out = open( '/Users/ionchannel/research/projects/ionchannels/non-selective.k/geneset/out.mbh.e_val1','a')
+notE1_out = open( '/Users/ionchannel/research/projects/ionchannels/non-selective.k/geneset/out.not.mbh.e_val1','a')
+mbhE001_out = open( '/Users/ionchannel/research/projects/ionchannels/non-selective.k/geneset/out.mbh.e_val0.01','a')
+notE001_out = open( '/Users/ionchannel/research/projects/ionchannels/non-selective.k/geneset/out.not.mbh.e_val0.01','a')
 
 ###parse 1###
 
@@ -32,66 +32,67 @@ is_top = True
 
 #for e_value 10
 for line in blastE10_in:
+
 	lineSplit = line.split(' ')
-		lineSplit = line.split(' ')
 	if lineSplit[0] == 'Query=': #if the script finds a query, it sets hsa_input_query to the last part of the line
 		is_top = True
-		hsa_input_query = lineSplit[-1]
+		hsa_input_query = lineSplit[-1][:-1]
 		oct_input_query = lineSplit[1]
 	#if the line is a result and is_top is True, then the result's id is appended onto blast_list
 	if lineSplit[0][0:3] == 'sp|' and is_top == True:
 		hsa_query_top_hit = lineSplit[0]
 		if hsa_input_query == hsa_query_top_hit:
-			output = hsa_input_query + ' -> ' + oct_input_query + ' -> ' + hsa_query_top_hit 
+			print 'yay'
+			output = hsa_input_query + ' -> ' + oct_input_query + ' -> ' + hsa_query_top_hit + '\n'
 			mbhE10_out.write(output)
 		if hsa_input_query == hsa_query_top_hit:
-			output = hsa_input_query + ' -> ' + oct_input_query + ' -> ' + hsa_query_top_hit
+			output = hsa_input_query + ' -> ' + oct_input_query + ' -> ' + hsa_query_top_hit + '\n'
 			notE10_out.write(output)
 		is_top = False
 		
 #for e_value 1		
 for line in blastE1_in:
 	lineSplit = line.split(' ')
-		lineSplit = line.split(' ')
+
 	if lineSplit[0] == 'Query=': #if the script finds a query, it sets hsa_input_query to the last part of the line
 		is_top = True
-		hsa_input_query = lineSplit[-1]
+		hsa_input_query = lineSplit[-1][:-1]
 		oct_input_query = lineSplit[1]
 	#if the line is a result and is_top is True, then the result's id is appended onto blast_list
 	if lineSplit[0][0:3] == 'sp|' and is_top == True:
 		hsa_query_top_hit = lineSplit[0]
 		if hsa_input_query == hsa_query_top_hit:
-			output = hsa_input_query + ' -> ' + oct_input_query + ' -> ' + hsa_query_top_hit 
+			output = hsa_input_query + ' -> ' + oct_input_query + ' -> ' + hsa_query_top_hit + '\n'
 			mbhE1_out.write(output)
 		if hsa_input_query == hsa_query_top_hit:
-			output = hsa_input_query + ' -> ' + oct_input_query + ' -> ' + hsa_query_top_hit
+			output = hsa_input_query + ' -> ' + oct_input_query + ' -> ' + hsa_query_top_hit + '\n'
 			notE1_out.write(output)
 		is_top = False
 		
 		
 #for e_value 0.01	
 for line in blastE001_in:
+
 	lineSplit = line.split(' ')
-		lineSplit = line.split(' ')
 	if lineSplit[0] == 'Query=': #if the script finds a query, it sets hsa_input_query to the last part of the line
 		is_top = True
-		hsa_input_query = lineSplit[-1]
+		hsa_input_query = lineSplit[-1][:-1]
 		oct_input_query = lineSplit[1]
 	#if the line is a result and is_top is True, then the result's id is appended onto blast_list
 	if lineSplit[0][0:3] == 'sp|' and is_top == True:
 		hsa_query_top_hit = lineSplit[0]
 		if hsa_input_query == hsa_query_top_hit:
-			output = hsa_input_query + ' -> ' + oct_input_query + ' -> ' + hsa_query_top_hit 
+			output = hsa_input_query + ' -> ' + oct_input_query + ' -> ' + hsa_query_top_hit + '\n'
 			mbhE001_out.write(output)
 		if hsa_input_query == hsa_query_top_hit:
-			output = hsa_input_query + ' -> ' + oct_input_query + ' -> ' + hsa_query_top_hit
+			output = hsa_input_query + ' -> ' + oct_input_query + ' -> ' + hsa_query_top_hit + '\n'
 			notE001_out.write(output)
 		is_top = False
 		
 #close files
-blastE10_in = 
-blastE1_in = 
-blastE001_in = 
+blastE10_in.close() 
+blastE1_in.close()
+blastE001_in.close()
 
 ###IOout
 
