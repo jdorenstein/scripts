66562dca81b4958be0e39aabd62abb737eac3b1a
Jeffery Orenstein <ionchannel@Erics-iMac.local>
2013-07-22 13:07:19 -0700

    script works

--- database.remove.duplicate.genes.13.07.22.py
+++ database.remove.duplicate.genes.13.07.22.py
@@ -1,14 +1,30 @@
 from Bio.Seq import Seq
 from Bio.Alphabet import IUPAC
+import os
 
 #this program is designed to parse a fasta file (ensembl), and remove all peptides, except for the longest, that are created from the same gene
+for fileName in os.listdir('/Users/ionchannel/research/tools/db/blast/12.proteomes/000.origional.docs/'): #list the ionchannel folders that the user can enter
+	print fileName
+input_name = raw_input('Enter the name of the file you want to format')
+#output_name = raw_input('Enter the name of the output file')
+output_name = input_name[:-2] + 'longest.peptide.fa'
+input_path = '/Users/ionchannel/research/tools/db/blast/12.proteomes/000.origional.docs/' + input_name
+output_path = '/Users/ionchannel/research/tools/db/blast/12.proteomes/000.origional.docs/' + output_name
+###IOin###
+#nothing to import
+###IOout###
+output_fasta = open(output_path,'a')
+
+
 #declare variables
 
 seq_dict = {} #format: [key-(gene locus)] : {[seq_record object], [seq length]}
 
+###parse 1###
+#create a dictionary that contains the sequences that should be kept
 
 from Bio import SeqIO
-for seq_record in SeqIO.parse("/Users/ionchannel/candidate.geneset.mapped.fa", "fasta"): #parses each fasta entry
+for seq_record in SeqIO.parse(input_path, "fasta"): #parses each fasta entry
 	#for each entry, check to see if the gene id has already been found. if so, check to see if the current gene is longer than the recorded gene (use a dictionary)
 	geneID_line = seq_record.description
 	geneID_list = geneID_line.split(' ')
@@ -26,7 +42,16 @@ for seq_record in SeqIO.parse("/Users/ionchannel/candidate.geneset.mapped.fa", "
 		seq_dict[geneID_list[4]] = [seq_record,sequence_length]
 print 'created dictionary'
 
-for key in seq_dict:
-	print seq_dict[key][0].format('fasta')
+
 			
+###parse 2###
+#write the dictionary to a file
+
+for key in seq_dict:
+	output = seq_dict[key][0].format('fasta')
+	output_fasta.write(output)
+	
+	
+output_fasta.close()
+
    
