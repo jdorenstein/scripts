00f89071f882f492ce6de811bdd01acb4c90df20
Jeffery Orenstein <ionchannel@Erics-iMac.local>
2013-07-23 15:30:20 -0700

    cleaned up the scripts folder (archived old scripts)

--- list.headers.13.07.22.py
+++
@@ -1,5 +0,0 @@
-#this script is designed to list the first line of all the files in the 13.proteomes library
-
-for fileName in os.listdir('/Users/ionchannel/research/tools/db/blast/13.proteomes/000.origional.docs/'): #list the ionchannel folders that the user can enter
-	if fileName[0] == 'p':
-		str = open(
